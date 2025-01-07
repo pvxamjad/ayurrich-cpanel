ba72dea891cf2eb94ae705ef0fdc27b75a216b8c
Amjad PV <amjadpvamd10@gmail.com>
2025-01-07 13:33:30 +0530

    static

--- ayur_rich/settings.py
+++ ayur_rich/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
